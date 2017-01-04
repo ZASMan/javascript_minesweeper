0a6ac26a7551a8bc34c4f9a6fcd50bd598642ced
zane <i.zane.am.robot@gmail.com>
2017-01-04 15:41:02 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+
+//Initial planning
+
+var game = {
+
+	intiateBoard = function() {
+		//Randomly place all of the mines
+		//If a square has a mine around it,
+	},
+
+	gameBoard: {
+		row1: [],
+		row2: [],
+		row3: [],
+		row4: [],
+		row5: [],
+		row6: [],
+		row7: [],
+		row8: [],
+		row9:[],
+	},
+
+	gameTime: 0,
+
+	selectSquare = function() {
+		//Listen for a click event
+		//if that square has an associated 
+	},
+
+
+
+
+}
+
+game.initiateBoard();
