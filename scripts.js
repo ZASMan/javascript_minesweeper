a3ba4e2c6033330e3887c38572602db5fc440f55
zane <i.zane.am.robot@gmail.com>
2017-01-04 20:17:55 -0500

    stuff

--- scripts.js
+++ scripts.js
@@ -1,35 +1,47 @@
-
 //Initial planning
 
 var game = {
 
-	intiateBoard = function() {
-		//Randomly place all of the mines
-		//If a square has a mine around it,
-	},
+	//Each array is one row
+	gameBoard: [
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],
+		[0,1,2,3,4,5,6,7,8,9],	
+	],
 
-	gameBoard: {
-		row1: [],
-		row2: [],
-		row3: [],
-		row4: [],
-		row5: [],
-		row6: [],
-		row7: [],
-		row8: [],
-		row9:[],
+	initiateBoard: function() {
+		//Randomly place all of the mine
+		//If a square has a mine around it,
+		//First loop through the board rows
+		for (row = 0; row < game.gameBoard.length; row++) {
+			//Loop through each row element
+			var randNum = Math.random() * (9 - 0) + 0;
+			var mine = "mine";
+			//Map each array, if random number is equal to
+			//the number index of the array, make it equal to "mine"
+			row.forEach(function(square, index) {
+				if (row[index] === randNum) {
+					row[index] = mine;
+				} else {
+					row[index] = "";
+				};
+			});
+		};
 	},
 
 	gameTime: 0,
 
-	selectSquare = function() {
-		//Listen for a click event
-		//if that square has an associated 
+	selectSquare: function() {
+		//Check if square has a mine
 	},
 
-
-
-
-}
+};
 
 game.initiateBoard();
+
