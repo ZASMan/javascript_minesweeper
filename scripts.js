a93073ba4fa81c241a0f355b722a1da8f0815111
zane <i.zane.am.robot@gmail.com>
2017-01-05 01:20:34 -0500

    stuff

--- scripts.js
+++ scripts.js
@@ -1,30 +1,16 @@
-//Initial planning
-
 var game = {
 
 	//Each array is one row
 	gameBoard: [
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],
-		[0,1,2,3,4,5,6,7,8,9],	
+		[1,2,3,4,5,6,7,8,9],
+
 	],
 
 	initiateBoard: function() {
-		//Randomly place all of the mine
-		//If a square has a mine around it,
-		//First loop through the board rows
-		for (row = 0; row < game.gameBoard.length; row++) {
+		for (row = 0; row < this.gameBoard.length; row++) {
 			//Loop through each row element
-			var randNum = Math.random() * (9 - 0) + 0;
+			var randNum = Math.random() * 9) + 1;
 			var mine = "mine";
-			//Map each array, if random number is equal to
-			//the number index of the array, make it equal to "mine"
 			row.forEach(function(square, index) {
 				if (row[index] === randNum) {
 					row[index] = mine;
